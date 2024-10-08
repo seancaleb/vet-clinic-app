b0b087c591514056289ec04b1304c21c0d4e706d
Sean Caleb <scmubias@gmail.com>
2024-10-08 18:22:42 +0800

    refactor: refactor code + update ui for auth routes

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+    <x-slot:header>Verify Email</x-slot:header>
+    <x-slot:header_text>Thanks for signing up! Before getting started, could you verify your email address by clicking
+        on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.</x-slot:header_text>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
@@ -9,22 +9,23 @@
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
+    <div class="mt-6 flex items-center justify-between">
         <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
             <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                <x-ui.primary-button>
+                    Resend Verification Email
+                </x-ui.primary-button>
             </div>
         </form>
 
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
+            <button type="submit"
+                class="underline text-sm text-gray-500 hover:text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                {{ __('Log out') }}
             </button>
         </form>
     </div>
