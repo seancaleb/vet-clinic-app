6872a3438e38fa8a7fb6825f871cadfedbcb7eac
Sean Caleb <scmubias@gmail.com>
2024-10-08 18:22:18 +0800

    refactor: refactor code + update ui for users routes

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,17 +1,22 @@
+@php
+    $role_options = ['admin', 'patient'];
+@endphp
+
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit User') }}
-        </h2>
-    </x-slot>
+    <x-slot:header>
+        Manage user
+    </x-slot:header>
 
-    <section class='section max-w-xl mx-auto'>
-        <div class='grid gap-1'>
-            <div class='text-gray-900 text-lg font-medium'>Edit User</div>
-            <p class='text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur harum
-                sequi aliquid officiis.</p>
-        </div>
 
+    <section class='section mx-auto max-w-xl'>
+        <header>
+            <h2 class="text-lg font-medium text-gray-800">
+                Manage user #{{ $user->id }}
+            </h2>
+            <p class="mt-1 text-sm text-gray-500">
+                Modify user details.
+            </p>
+        </header>
 
         <form method="POST" action="{{ route('users.update', ['user' => $user]) }}" class="mt-6 space-y-6">
             @csrf
@@ -19,32 +24,29 @@
 
             <div>
                 <x-input-label for="name" :value="__('Name')" />
-                <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" required
-                    value="{{ $user->name }}" />
+                <x-ui.input-text id="name" name="name" type="text" value="{{ $user->name }}" required />
                 <x-input-error class="mt-2" :messages="$errors->get('name')" />
             </div>
 
             <div>
                 <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" required
-                    value="{{ $user->email }}" />
+                <x-ui.input-text id="email" name="email" type="email" value="{{ $user->email }}" required />
                 <x-input-error class="mt-2" :messages="$errors->get('email')" />
             </div>
 
             <div>
                 <x-input-label for="role" class="block mb-2 text-sm font-medium">Role</x-input-label>
-                <select id="role" name="role" required value=""
-                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
-                    <option disabled>Select role</option>
-                    <option value="admin" @selected($user->role === 'admin')>Admin</option>
-                    <option value="patient" @selected($user->role === 'patient')>Patient</option>
-                </select>
+                <x-ui.input-select :id="'role'" :name="'role'" :defaultSelectedTitle="'Select role'" :options="$role_options"
+                    :selected="$user->role" />
+                <x-input-error class="mt-2" :messages="$errors->get('role')" />
             </div>
 
-            <div class="flex items-center gap-4 justify-between">
-                <x-link href="{{ route('users.show', ['user' => $user]) }}">{{ __('Back') }}</x-link>
-                <x-primary-button>{{ __('Update User') }}</x-primary-button>
-            </div>
+
+            <x-ui.primary-button class="flex items-center gap-2"><svg xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 text-white" viewBox="0 0 24 24">
+                    <path fill="currentColor"
+                        d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h11.175q.4 0 .763.15t.637.425l2.85 2.85q.275.275.425.638t.15.762V19q0 .825-.587 1.413T19 21zM19 7.85L16.15 5H5v14h14zM12 18q1.25 0 2.125-.875T15 15t-.875-2.125T12 12t-2.125.875T9 15t.875 2.125T12 18m-5-8h7q.425 0 .713-.288T15 9V7q0-.425-.288-.712T14 6H7q-.425 0-.712.288T6 7v2q0 .425.288.713T7 10M5 7.85V19V5z" />
+                </svg>Update User</x-ui.primary-button>
         </form>
     </section>
 </x-app-layout>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,22 @@
 @php
     use Carbon\Carbon;
+
+    function format_date($date)
+    {
+        return Carbon::parse($date)->format('m/d/Y');
+    }
+
+    function get_total_appointments($count)
+    {
+        if ($count === 0) {
+            return 'N/A';
+        } elseif ($count === 1) {
+            return "{$count} appointment";
+        } else {
+            return "{$count} appointments";
+        }
+    }
+
 @endphp
 
 <x-app-layout>
@@ -7,8 +24,8 @@
 
     <section class="space-y-6">
         <div class="space-y-6">
-            @if ($users->count() > 0)
-                <table class="bg-white rounded-xl overflow-clip">
+            @if ($users->count() > 1)
+                <table>
                     <tr>
                         <th>Name</th>
                         <th>Email</th>
@@ -23,11 +40,11 @@
                         @endif
 
                         <tr onclick="window.location='{{ route('users.show', ['user' => $user]) }}'">
-                            <td class='whitespace-nowrap text-gray-800 font-medium'>{{ $user->name }}</td>
+                            <td class='text-gray-800 font-medium'>{{ $user->name }}</td>
                             <td class='whitespace-nowrap'>{{ $user->email }}</td>
                             <td class='whitespace-nowrap'>{{ ucfirst($user->role) }}</td>
-                            <td class='whitespace-nowrap'>{{ $user->appointments->count() }}</td>
-                            <td class='whitespace-nowrap'>{{ Carbon::parse($user->created_at)->format('Y-m-d') }}
+                            <td class='whitespace-nowrap'>{{ get_total_appointments($user->appointments->count()) }}</td>
+                            <td class='whitespace-nowrap'>{{ format_date($user->created_at) }}
                             </td>
                             <td>
                                 @if (empty($user->email_verified_at))
@@ -42,9 +59,14 @@
                     @endforeach
                 </table>
             @else
-                <div class='px-6 py-8 rounded-lg bg-indigo-50/50 text-center'>
-                    <p>There are no active user/s listed at the moment.</p>
-                </div>
+                <section class="py-32 w-full text-gray-500">
+                    <div class="grid gap-2 text-center w-full justify-items-center">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-none tracking-[-0.015em]">
+                            No active users at the moment.
+                        </h2>
+                        <p class="max-w-[50ch]">There are currently no active patients yet.</p>
+                    </div>
+                </section>
 
             @endif
         </div>
