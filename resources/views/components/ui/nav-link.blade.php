336c5bae7555d7ad4d4cb35fd709ab6513cc381b
Sean Caleb <scmubias@gmail.com>
2024-10-08 18:19:14 +0800

    refactor: code refactor + update styles

--- resources/views/components/ui/nav-link.blade.php
+++ resources/views/components/ui/nav-link.blade.php
@@ -3,8 +3,8 @@
 @php
     $classes =
         $active ?? false
-            ? 'flex items-center gap-3 text-gray-500 font-medium text-sm px-4 py-2 rounded-lg bg-gray-100 text-gray-800'
-            : 'flex items-center gap-3 text-gray-500 font-medium text-sm px-4 py-2 rounded-lg';
+            ? 'flex h-10 items-center gap-3 text-gray-500 font-medium tracking-[-0.02em] text-[0.9375rem] px-4 py-2 rounded-lg bg-gray-50 text-gray-800'
+            : 'flex h-10 items-center gap-3 text-gray-500 font-medium tracking-[-0.02em] text-[0.9375rem] px-4 py-2 rounded-lg';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
