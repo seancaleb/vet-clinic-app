a7ab4a1e2c49a40390a93867a858adc951227a95
Sean Caleb <scmubias@gmail.com>
2024-10-09 21:41:05 +0800

    style: minor style changes

--- resources/views/components/ui/input-radio.blade.php
+++ resources/views/components/ui/input-radio.blade.php
@@ -7,7 +7,7 @@
         'value' => $value,
         'name' => $name,
         'class' =>
-            'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600',
+            'w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600',
     ]) !!}>
     <label {!! $attributes->merge([
         'for' => $labelFor,
